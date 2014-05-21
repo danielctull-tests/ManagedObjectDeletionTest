3d6e7270cea61f1687d899e165ba67dbc9e13e96
Daniel Tull <dt@danieltull.co.uk>
2014-05-21 14:24:48 +0100

    Initial commit

---
+++ ManagedObjectDeletionTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ManagedObjectDeletionTest
+//
+//  Created by Daniel Tull on 21.05.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+@import UIKit;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ManagedObjectDeletionTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ManagedObjectDeletionTest
+//
+//  Created by Daniel Tull on 21.05.2014.
+//  Copyright (c) 2014 Daniel Tull. All rights reserved.
+//
+
+@import UIKit;
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
